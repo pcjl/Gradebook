1b528ee64fc5f7d7531c576d0d3da274084577d4
philipliu <hellophilipliu@gmail.com>
2016-09-02 00:54:06 -0400

    Add save button to AddCourseActivity

--- app/src/main/java/us/philipli/gradebook/activities/AddCourseActivity.java
+++ app/src/main/java/us/philipli/gradebook/activities/AddCourseActivity.java
@@ -1,8 +1,13 @@
 package us.philipli.gradebook.activities;
 
+import android.support.annotation.NonNull;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import us.philipli.gradebook.R;
 
@@ -22,14 +27,14 @@ public class AddCourseActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        if (myToolbar == null) return;
+        if (myToolbar != null) {
+            // Hide title
+            this.getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        // Hide title
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        // Add close button
-        this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
-        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            // Add close button
+            this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
+            this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     @Override
@@ -37,5 +42,29 @@ public class AddCourseActivity extends AppCompatActivity {
 
         finish(); // close this activity as oppose to navigating up
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Add save button
+        final MenuItem menuItem = menu.add(Menu.NONE, 0, Menu.NONE, R.string.action_save);
+        MenuItemCompat.setShowAsAction(menuItem, MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case 0:
+                // save
+                Toast.makeText(AddCourseActivity.this, "Data saved",
+                        Toast.LENGTH_LONG).show();
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/us/philipli/gradebook/activities/MainActivity.java
+++ app/src/main/java/us/philipli/gradebook/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -65,5 +66,13 @@ public class MainActivity extends AppCompatActivity {
         // specify an adapter (see also next example)
         mAdapter = new CourseAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_main; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
